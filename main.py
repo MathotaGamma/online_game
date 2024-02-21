a5535316884da6c9b898935fb85000083f91dec6
MathotaGamma <159739543+MathotaGamma@users.noreply.github.com>
2024-02-21 21:57:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('./home_page.html')
+  return render_template('./home_page.html')
 
-app.run(port=8000, debug=True)
+app.run(host="0.0.0.0", port=5000, debug=True)
